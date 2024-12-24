d1d0f80795413bcfb1e10391bdc71b080945c21e
teklu abayneh <tekluabayneh@gmail.com>
2024-12-24 10:20:55 +0300

    Commit changes to modified script, index.html, and docs pages

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,33 @@
 let express = require("express");
 const cors = require("cors");
-
 const app = express();
+const postRouter = require("./routes/postes/postRoute");
+const userRoute = require("./routes/users/userRoute");
+const quoteRouter = require("./routes/quote/quoteRouter");
+const productRouter = require("./routes/product/productRouter");
+const Api_and_user_Route = require("./userApi_And_email/Route");
+const middleware = require("./middleWare/middleWare");
+
 let port = 8000;
+
 /* middleware */
 app.use(express.json());
 app.use(cors({ origin: true }));
-
-
-
-
-
-
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json("good start");
 });
 
+//  routes that don't need middleware before the global middleware
+app.use("/user_apikey", Api_and_user_Route);
 
+//middleware
+app.use(middleware);
 
-
+app.use("/posts", postRouter);
+app.use("/users", userRoute);
+app.use("/quotes", quoteRouter);
+app.use("/product", productRouter);
 
 app.listen(port, (err) => {
   if (err) {
